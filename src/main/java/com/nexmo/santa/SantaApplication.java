1195e697ff35b8f594e7cf288b68c0446efd0550
Steve Crow <steve.crow@vonage.com>
2018-11-26 12:56:26 -0500

    Register Nexmo client bean for injection.

--- src/main/java/com/nexmo/santa/SantaApplication.java
+++ src/main/java/com/nexmo/santa/SantaApplication.java
@@ -1,12 +1,19 @@
 package com.nexmo.santa;
 
+import com.nexmo.client.NexmoClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SantaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SantaApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(SantaApplication.class, args);
-	}
+    @Bean
+    public NexmoClient getNexmoClient(@Value("${nexmo.api.key}") String key, @Value("${nexmo.api.secret}") String secret) {
+        return new NexmoClient.Builder().apiKey(key).apiSecret(secret).build();
+    }
 }
