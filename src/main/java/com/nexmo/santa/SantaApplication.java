6b8775e9df577bd885ce7bf7d8c6b1859ddd52cf
Steve Crow <steve.crow@vonage.com>
2018-11-26 10:10:53 -0500

    Initialize Spring Boot project.

---
+++ src/main/java/com/nexmo/santa/SantaApplication.java
@@ -0,0 +1,12 @@
+package com.nexmo.santa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantaApplication.class, args);
+	}
+}
