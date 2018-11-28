1dfc7d7fdc063c299db83f638a269b85520516d1
Steve Crow <steve.crow@vonage.com>
2018-11-28 15:02:46 -0500

    Seed information from Google's API on initialization.

--- src/main/java/com/nexmo/santa/SantaApplication.java
+++ src/main/java/com/nexmo/santa/SantaApplication.java
@@ -1,10 +1,14 @@
 package com.nexmo.santa;
 
 import com.nexmo.client.NexmoClient;
+import com.nexmo.santa.location.SantaLocationLookupService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class SantaApplication {
@@ -12,8 +16,17 @@ public class SantaApplication {
         SpringApplication.run(SantaApplication.class, args);
     }
 
+    @Autowired
+    private SantaLocationLookupService santaLocationLookupService;
+
     @Bean
     public NexmoClient getNexmoClient(@Value("${nexmo.api.key}") String key, @Value("${nexmo.api.secret}") String secret) {
         return new NexmoClient.Builder().apiKey(key).apiSecret(secret).build();
     }
+
+    @EventListener
+    public void seed(ContextRefreshedEvent event) {
+        santaLocationLookupService.seedLocations();
+    }
+
 }
